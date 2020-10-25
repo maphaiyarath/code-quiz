09a821fef612d185ffd25ccb8d6a48dbe63aee93
chelle <maphaiyarath@gmail.com>
2020-10-24 19:11:32 -0500

    added score to localStorage

--- script.js
+++ script.js
@@ -1,64 +1,74 @@
-// WHEN all questions are answered or the timer reaches 0 THEN the game is over
-// WHEN the game is over THEN I can save my initials and score
-
+//
 var alertsEl = document.querySelector(".alerts");
 var welcome = document.getElementById("welcome");
 var quiz = document.getElementById("quiz justify-content-around text-center");
 var doneEl = document.getElementById("done justify-content-around text-center");
-var scores = document.getElementById("highscores");
 var yourScore = document.getElementById("your-score");
 var timeEl = document.getElementById("time");
 var correctEl = document.querySelector(".alert-success");
 var wrongEl = document.querySelector(".alert-danger");
+
+
 var questionIndex = 0;
 var score = 0;
 var timeLeft = 0;
 
 
 
-var questions = [
-    {q: 'What does the terminal command \'cd\' do?', a: 'Change the directory', choices: ['Change the directory', 'Copy the directory', 'Play a CD']},
-    {q: 'Which style positions elements relative to their nearest positioned ancestor?', a: 'Absolute', choices: ['Absolute', 'Relative', 'Fixed']}
+var questions = [{
+        q: 'What does the terminal command \'cd\' do?',
+        a: 'Change the directory',
+        choices: ['Change the directory', 'Copy the directory', 'Play a CD']
+    },
+    {
+        q: 'Which style positions elements relative to their nearest positioned ancestor?',
+        a: 'Absolute',
+        choices: ['Absolute', 'Relative', 'Fixed']
+    },
+    {
+        q: 'What does the alt attribute in an <img> tag?',
+        a: 'Specifies alternate text for an image if it cannot be displayed',
+        choices: ['Provides an alternate image to display', 'Specifies the path to the image', 'Specifies alternate text for an image if it cannot be displayed']
+    },
+    {
+        q: 'What is the value of x in the following line of code? \nvar x = "Volvo" + 16 + 4;',
+        a: 'Volvo164',
+        choices: ['Volvo20', 'Volvo164', 'undefined']
+    }
 ];
 
 
 
-function clearScores() {
-    scores.innerHTML = '';
-}
 
 // start the quiz when the start button is clicked
 function startQuiz() {
     // clear start quiz info
     welcome.setAttribute("style", "display: none;");
     quiz.setAttribute("style", "display: block");
-    isPlaying = true;
+
     // the timer starts
     startTimer();
-    
+
     // the user is presented with a question
     selectNextQ();
-    
-        
+
+
 }
 
 function selectNextQ() {
     quiz.innerHTML = '';
     if (questionIndex < questions.length) {
-        
-
-        // create a card for each question
         var questionEl = document.createElement("div");
         questionEl.setAttribute("class", "question");
 
-        // card's title will be the question
+        // the question
         var questionQ = document.createElement("h3");
         questionQ.textContent = questions[questionIndex].q;
         questionEl.append(questionQ);
 
         quiz.append(questionEl);
 
-        // card's body will be the choices
+        // display the choices as buttons
         for (var i = 0; i < questions[questionIndex].choices.length; i++) {
             var choice = document.createElement("button");
             choice.setAttribute("class", "btn btn-primary");
@@ -81,7 +91,6 @@ function selectNextQ() {
 }
 
 function chooseAnswer(value) {
-    // TODO: keep track of score
     if (value.dataset.value === value.dataset.answer) {
         correctEl.setAttribute("style", "display: block");
         wrongEl.setAttribute("style", "display: none");
@@ -89,7 +98,7 @@ function chooseAnswer(value) {
             correctEl.style.display = "none";
         }, 1000);
         score += 10;
-        
+
     } else {
         // alert the user that they are wrong
         wrongEl.setAttribute("style", "display: block");
@@ -99,12 +108,12 @@ function chooseAnswer(value) {
         }, 1000);
 
         // when the user answers a question incorrectly, then time is subtracted from the clock
-        timeLeft -= 10;
+        timeLeft -= 9;
     }
 
 
 
-   questionIndex++;
+    questionIndex++;
 
     // when the user answers a question, then they are presented with another question
     selectNextQ();
@@ -127,16 +136,26 @@ function startTimer() {
             if (timeLeft <= 4) {
                 timeEl.style.color = 'red';
             }
-            if (timeLeft <= 0) {
+            if (timeLeft < 0) {
                 // when the timer reaches 0, then the game is over
                 gameOver();
                 timeLeft = 0;
 
-                
+
             }
         }, 1000);
-        
+
     } else {
         timeEl.textContent = 0;
     }
+}
+
+// when the game is over, the user can save their initials and score
+function saveScore() {
+    var initials = document.getElementById("initialInput");
+    var userScore = {
+        'initials': initials.value,
+        'score': score
+    };
+    localStorage.setItem('scores', JSON.stringify(userScore));
 }
